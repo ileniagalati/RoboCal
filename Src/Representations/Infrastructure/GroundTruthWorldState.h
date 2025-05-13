b7e931086ff66d3b5a9a150953d93d4796bec0b0
ilenia <ilenia.galati@unical.it>
2025-05-13 12:54:07 +0200

    HalfFieldBH.ros2 simulation, fixed position wrong translation

--- Src/Representations/Infrastructure/GroundTruthWorldState.h
+++ Src/Representations/Infrastructure/GroundTruthWorldState.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f4473ea97c1e4a48047a8ab98b007a1333385efc9ca49f8f4c54b18205180ab
+oid sha256:65034e1b7474a034de3505d145bfdbd2b9b95c9eecd7f70f48e98f9eea465c94
 size 827
